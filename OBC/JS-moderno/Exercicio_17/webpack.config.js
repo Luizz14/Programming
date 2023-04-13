77acf4401299960d9a37a0353a72aa461736da47
LuizPC <gustavo80moura@gmail.com>
2023-04-13 12:08:08 -0300

    Commit "Finalizando modulo JS-moderno"

---
+++ OBC/JS-moderno/Exercicio_17/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    }
+  },
+  entry: {
+    index: './src/index.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js'
+  }
+}
